87f7cd6f39241ac474b023413ec41e1109728742
luiz-otavio <luizfarias2006@hotmail.com>
2022-11-05 14:28:51 -0300

    refactor(api): change exceptions to be `RuntimeException`

--- api/src/main/java/me/luizotavio/minecraft/exception/ServerDoesntExistException.java
+++ api/src/main/java/me/luizotavio/minecraft/exception/ServerDoesntExistException.java
@@ -4,7 +4,7 @@ package me.luizotavio.minecraft.exception;
  * @author Luiz O. F. Corrêa
  * @since 02/11/2022
  **/
-public class ServerDoesntExistException extends Exception {
+public class ServerDoesntExistException extends RuntimeException {
 
     public ServerDoesntExistException(String message) {
         super(message);
--- api/src/main/java/me/luizotavio/minecraft/exception/UserDoesntExistException.java
+++ api/src/main/java/me/luizotavio/minecraft/exception/UserDoesntExistException.java
@@ -4,7 +4,7 @@ package me.luizotavio.minecraft.exception;
  * @author Luiz O. F. Corrêa
  * @since 02/11/2022
  **/
-public class UserDoesntExistException extends Exception {
+public class UserDoesntExistException extends RuntimeException {
 
     public UserDoesntExistException(String message) {
         super(message);
