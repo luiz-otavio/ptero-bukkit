3e03c721c94266c66a4f5c527eb010e52cca232d
luiz-otavio <luizfarias2006@hotmail.com>
2022-12-07 16:38:30 -0300

    fix(binder): forgot to resolve constructor on ptero user impl

--- binder/src/main/java/net/luxcube/minecraft/user/PteroUserImpl.java
+++ binder/src/main/java/net/luxcube/minecraft/user/PteroUserImpl.java
@@ -102,7 +102,8 @@ public class PteroUserImpl implements PteroUser {
                     addressAndNode.first(),
                     addressAndNode.second(),
                     server.getName(),
-                    server.getUUID()
+                    server.getUUID(),
+                    null
                 );
 
                 pteroServers.add(targetServer);
