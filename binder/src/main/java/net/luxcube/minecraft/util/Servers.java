c43dde41fc4e6f9cd0f6434e86c9dfc8caa7ba78
luiz-otavio <luizfarias2006@hotmail.com>
2023-01-06 21:21:37 -0300

    fix(binder): ensure the right connection address

--- binder/src/main/java/net/luxcube/minecraft/util/Servers.java
+++ binder/src/main/java/net/luxcube/minecraft/util/Servers.java
@@ -4,6 +4,7 @@ import com.mattmalec.pterodactyl4j.application.entities.ApplicationAllocation;
 import com.mattmalec.pterodactyl4j.application.entities.ApplicationServer;
 import com.mattmalec.pterodactyl4j.client.entities.ClientAllocation;
 import com.mattmalec.pterodactyl4j.client.entities.ClientServer;
+import com.mattmalec.pterodactyl4j.entities.Allocation;
 import org.jetbrains.annotations.Blocking;
 import org.jetbrains.annotations.NotNull;
 
@@ -15,27 +16,21 @@ import java.util.concurrent.TimeUnit;
  **/
 public class Servers {
 
+    private static final String LOCAL_IP = "127.0.0.1";
+    private static final String PUBLIC_IP = "0.0.0.0";
+
     @Blocking
     public static Pair<String, String> getAddressAndNode(@NotNull ApplicationServer applicationServer) {
         ApplicationAllocation allocation = applicationServer.retrieveDefaultAllocation()
             .timeout(5, TimeUnit.SECONDS)
             .execute();
 
-        String address = null,
-            node = null;
-        if (allocation == null) {
-            address = "";
-            node = "N/A";
-        } else {
-            address = allocation.getFullAddress();
+        String node = applicationServer.retrieveNode()
+            .timeout(5, TimeUnit.SECONDS)
+            .execute()
+            .getName();
 
-            node = applicationServer.retrieveNode()
-                .timeout(5, TimeUnit.SECONDS)
-                .execute()
-                .getName();
-        }
-
-        return new Pair<>(address, node);
+        return new Pair<>(ensureAddress(allocation), node);
     }
 
     @Blocking
@@ -43,9 +38,21 @@ public class Servers {
         ClientAllocation allocation = clientServer.getPrimaryAllocation();
 
         return new Pair<>(
-            allocation.getFullAddress(),
+            ensureAddress(allocation),
             clientServer.getNode()
         );
     }
+
+    private static String ensureAddress(@NotNull Allocation allocation) {
+        String address = allocation.getIP(),
+            port = allocation.getPort();
+
+        if (address.equalsIgnoreCase(LOCAL_IP) || address.equalsIgnoreCase(PUBLIC_IP)) {
+            address = allocation.getAlias();
+        }
+
+        return String.format("%s:%s", address, port);
+    }
+
 
 }
